4d851cea8442547126a3299ac5b592ccc0450aab
Stacy Lofton <lofton713@gmail.com>
2022-07-18 14:41:32 -0500

    initial commit

---
+++ dailyjournal.sql
@@ -0,0 +1,35 @@
+DROP TABLE IF EXISTS Journal_Entries;
+DROP TABLE IF EXISTS Moods;
+
+CREATE TABLE `Moods`(
+    `id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `label` TEXT NOT NULL
+    ADD CONSTRAINT FOREIGN KEY(`mood_id`) REFERENCES Moods(`id`);
+);
+
+CREATE TABLE `Journal_entries` (
+    `id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `concept`   TEXT NOT NULL,
+    `date`  DATE, 
+    `mood_id`   INTEGER NOT NULL
+    
+);
+
+
+INSERT INTO `Moods` VALUES ( NULL, "Happy");
+
+INSERT INTO `Moods` VALUES ( NULL, "Sad");
+INSERT INTO `Moods` VALUES ( NULL, "Mad");
+INSERT INTO `Moods` VALUES ( NULL, "Anxious");
+INSERT INTO `Moods` VALUES ( NULL, "Relieved");
+
+INSERT INTO `Journal_entries` VALUES ( NULL, "What is Python?", '2022-07-18', 4);
+
+INSERT INTO `Journal_entries` VALUES ( NULL, "Python is fun", '2022-07-19', 1);
+INSERT INTO `Journal_entries` VALUES ( NULL, "Python is hard", '2022-07-20', 2);
+INSERT INTO `Journal_entries` VALUES ( NULL, "Python is not too bad", '2022-07-21', 5);
+INSERT INTO `Journal_entries` VALUES ( NULL, "Python is impossible!!", '2022-07-22', 3);
+INSERT INTO `Journal_entries` VALUES ( NULL, "Python is fun, again! ", '2022-07-18', 1);
+
+CREATE TABLE `Mood_tags` (
+    `
---
+++ views/enrty_requests.py
@@ -0,0 +1,14 @@
+from http.server import HTTPServer
+
+# This function is not inside the class. It is the starting
+# point of this application.
+def main():
+    """Starts the server on port 8088 using the HandleRequests class
+    """
+    host = ''
+    port = 8088
+    HTTPServer((host, port), HandleRequests).serve_forever()
+
+
+if __name__ == "__main__":
+    main()
